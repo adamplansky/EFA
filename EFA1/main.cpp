1e31a00cda62f6b8cdb7b4da89781f984e5489b2
Adam Plansky <adamplansky@gmail.com>
2013-10-05 06:48:43 +0200

    test1

---
+++ EFA1/main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: plansada
+ *
+ * Created on 5. říjen 2013, 6:44
+ */
+
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "test" <<endl;
+    return 0;
+}
+
